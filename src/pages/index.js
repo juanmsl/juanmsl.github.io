6c504699fb428ce3610db3ed2ac7ad6637d4385e
Juan Sanchez <juanmsl_pk@hotmail.com>
2020-04-11 02:59:40 -0500

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,17 @@ class App extends React.Component {
                 <Section main navbar>
                     <Navbar/>
                     <section>
-                        <h4>Wellcome, I’m</h4>
+                        <h4>Welcome, I’m</h4>
                         <h1>Juan Manuel Sánchez</h1>
-                        <h3>Fullstack developer</h3>
+                        <h3>FullStack developer</h3>
                         <p>I’m a web developer who is passionate about what I do, programming and design, both of them are my hobbies and my livelihood, making things functional and visually attractive are some of my priorities.</p>
                     </section>
-                    <Image src='photo.svg'/>
+                    <Image src='sections/photo.svg'/>
                 </Section>
                 <Section>
-                    <Image src='proffesional_skills.svg' />
+                    <Image src='sections/professional_skills.svg' />
                     <section>
-                        <Title>Proffesional Skills</Title>
+                        <Title>Professional Skills</Title>
                         <p>I love what I do, and I’m constantly improving my knowledge and skills to be better each day.</p>
                     </section>
                 </Section>
@@ -27,10 +27,10 @@ class App extends React.Component {
                     <section>
                         <Title>My Experience</Title>
                     </section>
-                    <Image src='my_experience.svg' />
+                    <Image src='sections/my_experience.svg' />
                 </Section>
                 <Section blue_full>
-                    <Image src='services.svg' />
+                    <Image src='sections/services.svg' />
                     <section>
                         <Title>What I can do for you</Title>
                     </section>
--- src/shared/components/social-networks/index.js
+++ src/shared/components/social-networks/index.js
@@ -5,10 +5,21 @@ import {Image, LinkTo} from "shared/components";
 
 const SocialNetworks = ({dark}) => (
     <section className={`grid networks ${dark && 'dark'}`}>
-        <LinkTo to='https://www.linkedin.com/in/juanmsl/'><Image src={`icons/linkedin${dark ? '_dark' : ''}.svg`} className='networks__item'/></LinkTo>
-        <LinkTo to='https://github.com/juanmsl'><Image src={`icons/github${dark ? '_dark' : ''}.svg`} className='networks__item'/></LinkTo>
-        <LinkTo to='https://codepen.io/juanmsl'><Image src={`icons/codepen${dark ? '_dark' : ''}.svg`} className='networks__item'/></LinkTo>
-        <LinkTo to='mailto:juanmsl_pk@hotmail.com'><Image src={`icons/email${dark ? '_dark' : ''}.svg`} className='networks__item'/></LinkTo>
+        <LinkTo to='https://www.linkedin.com/in/juanmsl/'>
+            <Image src={`icons/linkedin${dark ? '_dark' : ''}.svg`} className='networks__item'/>
+        </LinkTo>
+
+        <LinkTo to='https://github.com/juanmsl'>
+            <Image src={`icons/github${dark ? '_dark' : ''}.svg`} className='networks__item'/>
+        </LinkTo>
+
+        <LinkTo to='https://codepen.io/juanmsl'>
+            <Image src={`icons/codepen${dark ? '_dark' : ''}.svg`} className='networks__item'/>
+        </LinkTo>
+
+        <LinkTo to='mailto:juanmsl_pk@hotmail.com'>
+            <Image src={`icons/email${dark ? '_dark' : ''}.svg`} className='networks__item'/>
+        </LinkTo>
     </section>
 );
 
